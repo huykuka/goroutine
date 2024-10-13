d1442b2192d2bf2e6042d427f791181e86e7a712
thaiqu <thaiquanghuy.c3a@gmail.com>
2024-10-13 23:46:42 +0700

    feat: done for sync.Wait Group

--- main.go
+++ main.go
@@ -5,27 +5,34 @@ import (
 	"sync"
 )
 
+var msg string
 
-//
-func main() {
-	var wg sync.WaitGroup
-
-	words := []string{
-		"alpha", "delta", "gamma",
-	}
-
-	wg.Add(len(words))
-	for i, v := range words {
-		go printsth(fmt.Sprintf("%d:%s", i, v), &wg)
-	}
-	wg.Wait()
-
-
+func updateMessage(s string) {
+	msg = s
 }
 
-func printsth(s string, wg *sync.WaitGroup) {
+func printMessage(wg *sync.WaitGroup) {
 	defer wg.Done()
+	fmt.Println(msg)
+}
 
+// The idea of wait group is to define a counter, where every registered goroutines go through have to decrease the counter to value 0 (wg.Wait())
+// To be noticed that the counter should be always greater than 0
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	updateMessage("Hello, universe!")
+	go printMessage(&wg)
+	wg.Wait()
 
-	fmt.Println(s)
+	wg.Add(1)
+	updateMessage("Hello, cosmos!")
+	go printMessage(&wg)
+	wg.Wait()
+
+	wg.Add(1)
+	updateMessage("Hello, world!")
+	go printMessage(&wg)
+	wg.Wait()
+
 }
